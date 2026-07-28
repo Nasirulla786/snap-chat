b374f5368818ee7461f4b689d705b065d1232116
nasirulla <rehmanansirulla1124@gmail.com>
2026-07-28 13:10:35 +0530

    Final bug fix - authentication bug

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 import os
@@ -24,6 +25,15 @@ cloudinary.config(
 )
 
 
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
+
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": False,
+}
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
